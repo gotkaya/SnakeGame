37ed1a335d1a3d3e4c871655f3d9e828aca9d13b
0 <elik@example.com>
2021-01-14 12:58:43 +0200

    first commit

---
+++ SnakeGame/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace SnakeGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int score = 0;
+            Walls walls = new Walls(80, 25);
+            walls.Draw();
+
+            Point snakeTail = new Point(15, 15, 's');
+            Snake snake = new Snake(snakeTail, 5, Direction.RIGHT);
+            snake.Draw();
+
+            FoodGenerator foodGenerator = new FoodGenerator(80, 25, '$');
+            Point food = foodGenerator.GenerateFood();
+            food.Draw();
+
+            while(true)
+            {
+                if(walls.IsHit(snake) || snake.IsHitTail())
+                {
+                    break;
+                }
+                if(snake.Eat(food))
+                {
+                    food = foodGenerator.GenerateFood();
+                    food.Draw();
+                    score++;
+                }else
+                {
+                    snake.Move();
+                }
+
+                if(Console.KeyAvailable)
+                {
+                    ConsoleKeyInfo key = Console.ReadKey();
+                    snake.HandleKays(key.Key);
+                }
+
+                Thread.Sleep(300);
+            }
+            string str_score = Convert.ToString(score);
+            WriteGameOver(str_score);
+            Console.ReadLine();
+        }
+        public static void WriteGameOver(string score)
+        {
+            int xOffset = 25;
+            int yOffset = 8;
+            Console.ForegroundColor = ConsoleColor.DarkBlue;
+            Console.SetCursorPosition(xOffset, yOffset++);
+            WriteText("==============================", xOffset, yOffset++);
+            WriteText("           GAMEOVER           ", xOffset+1, yOffset++);
+            yOffset++;
+            WriteText($"  YOU SCORED {score} points", xOffset+2, yOffset++);
+            WriteText("", xOffset+2, yOffset++);
+            WriteText("=============================", xOffset+2, yOffset++);
+
+        }
+
+        public static void WriteText(string text, int xOffset,int yOffset)
+        {
+            Console.SetCursorPosition(xOffset, yOffset);
+            Console.WriteLine(text);
+        }
+      
+    }
+}
